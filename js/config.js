8f3d69db4502a5588261a6a74436d242329e1e0a
张洪儒 <36877424+19860413@users.noreply.github.com>
2025-08-16 09:25:08 +0800

    冰痕

--- js/config.js
+++ js/config.js
@@ -13,9 +13,9 @@ const PASSWORD_CONFIG = {
 
 // 网站信息配置
 const SITE_CONFIG = {
-    name: 'LibreTV',
+    name: '冰痕TV',
     url: 'https://libretv.is-an.org',
-    description: '免费在线视频搜索与观看平台',
+    description: '冻结时光的影视胶囊',
     logo: 'image/logo.png',
     version: '1.0.3'
 };
